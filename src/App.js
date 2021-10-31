42dabf0ee77deaaeaba510f0424c53c902119e01
erdenedavaa <erdenedavaa@users.noreply.github.com>
2021-10-31 22:37:24 +0800

    Home finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Sidebar from './components/sidebar/Sidebar'
+import Topbar from './components/topbar/Topbar'
+import './app.css'
+import Home from './pages/home/Home'
+
 function App() {
-  return <div className="App">hello</div>
+  return (
+    <div className="fullContainer">
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
+    </div>
+  )
 }
 
 export default App
